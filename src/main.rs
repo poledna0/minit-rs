0550baa1aeade3c7df716c2241ccc7afbb9ae604
poledna0 <henriquepoledna@proton.me>
2025-12-27 18:14:48 -0300

    codigo para testar exec com pid 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    loop{
+        println!("Pato - Teste de codigo rodando com pid 1");
+        std::thread::sleep(std::time::Duration::from_secs(3));
+    }
 }
