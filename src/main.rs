12bae8d21c03e33f6d81e6312e2e442956a2b76a
poledna0 <henriquepoledna@proton.me>
2026-01-16 20:28:11 -0300

    Remove PID check and simplify console input handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use std::fs::OpenOptions;
 use std::io::{Write, Read};
-use std::process::exit;
 
 fn main() {
-    // Verificação simples
-    if unsafe { libc::getpid() } != 1 {
-        eprintln!("não sou PID 1");
-        exit(1);
-    }
 
-    // Abre o console diretamente
     let mut console = OpenOptions::new()
         .read(true)
         .write(true)
         .open("/dev/console")
         .expect("falha ao abrir /dev/console");
 
-    // Mensagem inicial
     writeln!(console, "INIT PID 1 rodando").ok();
     writeln!(console, "Digite algo e pressione Enter:\n").ok();
 
@@ -25,7 +17,6 @@ fn main() {
     loop {
         input.clear();
 
-        // Lê uma linha inteira
         let mut buf = [0u8; 1];
         while console.read(&mut buf).unwrap() == 1 {
             if buf[0] == b'\n' {
@@ -34,7 +25,6 @@ fn main() {
             input.push(buf[0] as char);
         }
 
-        // Repete
         writeln!(console, "{}", input).ok();
     }
 }
